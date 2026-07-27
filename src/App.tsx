ba54851d6d85641d64e89f9270aba45ff9a58125
dicey44 <226594125+dicey44@users.noreply.github.com>
2026-07-27 16:29:11 -0400

    feat: create search handler to filter among pokemon

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import { getList } from "./services/pokemon-list-fetch";
 import type { PokemonListing } from "./types/pokemon";
 import PokemonList from "./components/PokemonList";
-import { useEffect, useState } from "react";
+import { useEffect, useState} from "react";
 import "./App.css"
 
 
 function App() {
     const [pokemon, setPokemon] = useState<PokemonListing[]>([]);
+    const [query, setQuery] = useState("");
 
     useEffect(() => {
         async function loadPokemon() {
@@ -17,10 +18,19 @@ function App() {
         loadPokemon();
     }, []);
 
+    function handleSearchBar(e: React.ChangeEvent<HTMLInputElement>) {
+        setQuery(e.target.value);
+    }
+
+    const filteredPokemon = pokemon.filter(poke => poke.name.toLowerCase().includes(query.toLowerCase()));
+
+    
     return (
         <div>
             <h1 className="title">Kanto Pokedex</h1>
-            <PokemonList pokemon={pokemon} />
+            <input type="text" placeholder="Search Pokemon" className="search-bar" maxLength={25} value={query} onChange={handleSearchBar}></input>
+            <PokemonList pokemon={filteredPokemon} />
+            <h2 className={filteredPokemon.length === 0 ? "show center" : "hide"}>Pokemon not found</h2>
         </div>
     );
 }
