64a78801bd679fe34dcfa86cc8d2e82f4719d7c8
dicey44 <226594125+dicey44@users.noreply.github.com>
2026-08-02 14:39:14 -0400

    chore: add backdrop for pokemon modal

--- src/components/PokemonDetail.tsx
+++ src/components/PokemonDetail.tsx
@@ -15,42 +15,45 @@ export default function PokemonDetail({ selectedPokemon, closePokemonDetails }: 
     }
 
     return (
-        <div className="pokemon-detailed">
-            <div className="pokemon-details-container">
-                <p className="details-title">{selectedPokemon.name.charAt(0).toUpperCase() + selectedPokemon.name.slice(1)} ID#{selectedPokemon.id}</p>
-                <button onClick={closePokemonDetails}><span>X</span></button>
-                <img src={selectedPokemon.image_url} className="details-img"></img>
-                <audio controls className="details-audio">
-                    <source src={selectedPokemon.cry_url} type="audio/ogg"></source>
-                </audio>
-                {selectedPokemon.types.map((type: string) => (
-                    <span className={type.toLowerCase() + " details-type"} key={type.toLowerCase()}>{type.charAt(0).toUpperCase() + type.slice(1)}</span>
-                ))}
-                <div>
-                    <span>Hp: </span>
-                    <span>{selectedPokemon.stats.hp}</span>
-                </div>
-                <div>
-                    <span>Attack: </span>
-                    <span>{selectedPokemon.stats.attack}</span>
-                </div>
-                <div>
-                    <span>Defense: </span>
-                    <span>{selectedPokemon.stats.defense}</span>
-                </div>
-                <div>
-                    <span>Special Attack: </span>
-                    <span>{selectedPokemon.stats.specialAttack}</span>
-                </div>
-                <div>
-                    <span>Special Defense: </span>
-                    <span>{selectedPokemon.stats.specialDefense}</span>
-                </div>
-                <div>
-                    <span>Speed: </span>
-                    <span>{selectedPokemon.stats.speed}</span>
+        <div className="backdrop">
+            <div className="pokemon-detailed">
+                <div className="pokemon-details-container">
+                    <p className="details-title">{selectedPokemon.name.charAt(0).toUpperCase() + selectedPokemon.name.slice(1)} ID#{selectedPokemon.id}</p>
+                    <button onClick={closePokemonDetails}><span>X</span></button>
+                    <img src={selectedPokemon.image_url} className="details-img"></img>
+                    <audio controls className="details-audio">
+                        <source src={selectedPokemon.cry_url} type="audio/ogg"></source>
+                    </audio>
+                    {selectedPokemon.types.map((type: string) => (
+                        <span className={type.toLowerCase() + " details-type"} key={type.toLowerCase()}>{type.charAt(0).toUpperCase() + type.slice(1)}</span>
+                    ))}
+                    <div>
+                        <span>Hp: </span>
+                        <span>{selectedPokemon.stats.hp}</span>
+                    </div>
+                    <div>
+                        <span>Attack: </span>
+                        <span>{selectedPokemon.stats.attack}</span>
+                    </div>
+                    <div>
+                        <span>Defense: </span>
+                        <span>{selectedPokemon.stats.defense}</span>
+                    </div>
+                    <div>
+                        <span>Special Attack: </span>
+                        <span>{selectedPokemon.stats.specialAttack}</span>
+                    </div>
+                    <div>
+                        <span>Special Defense: </span>
+                        <span>{selectedPokemon.stats.specialDefense}</span>
+                    </div>
+                    <div>
+                        <span>Speed: </span>
+                        <span>{selectedPokemon.stats.speed}</span>
                 </div>
             </div>
         </div>
+        </div>
+       
     )
 }
